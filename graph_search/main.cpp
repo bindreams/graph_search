f08f9ec1478410788d1e1adcbefa2a7e1539f141
andreasxp <andres.zhukov@gmail.com>
2018-02-20 17:50:40 +0300

    Chenged test4 to test_from_file

--- graph_search/main.cpp
+++ graph_search/main.cpp
@@ -8,7 +8,7 @@
 int main() {
 	std::cout << std::setw(2) << std::setfill('0') << std::boolalpha;
 	std::cout << "Threads available: " << std::thread::hardware_concurrency() << std::endl;
-	//test4();
+	//test_from_file("save.json");
 
-	std::cout << random_graph(20, 0.8, 10);
+	std::cout << random_graph(1000, 0.2, 5);
 }
